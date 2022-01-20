d54d3251ddc00ee23f5ba6f9e53fae53dd960a71
Gautam Verma <vgautam310@gmail.com>
2022-01-20 15:41:18 +0530

    Jan18

---
+++ Jan18/ProgramOne.java
@@ -0,0 +1,29 @@
+
+public class ProgramOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Currently in the class ProgramOne.");
+		System.out.println("Our goal is to develop solutions and save time.");
+		System.out.println("Be Exceptional.");
+	}
+
+}
+
+class Two{
+	public static void main(String[] args)
+	{
+		System.out.println("Currently in class Two.");
+		System.out.println("Our goal is to develop solutions and save time.");
+		System.out.println("Be Exceptional.");
+	}
+}
+
+class Three{
+	public static void main(String [] args)
+	{
+		System.out.println("Currently in class Three.");
+		System.out.println("Our goal is to develop solutions and save time.");
+		System.out.println("Be Exceptional.");
+	}
+}
